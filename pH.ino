3bc4e8d140198703b59049dd4b08178481bfc3a3
Magnus Selle <97983004+MagnSell@users.noreply.github.com>
2024-02-06 12:21:31 +0100

    pH combined with temperature
    https://wiki.dfrobot.com/Gravity__Analog_pH_Sensor_Meter_Kit_V2_SKU_SEN0161-V2#More_Documents

---
+++ pH.ino
@@ -0,0 +1,65 @@
+/*
+ * file DFRobot_PH.ino
+ * @ https://github.com/DFRobot/DFRobot_PH
+ *
+ * This is the sample code for Gravity: Analog pH Sensor / Meter Kit V2, SKU:SEN0161-V2
+ * In order to guarantee precision, a temperature sensor such as DS18B20 is needed, to execute automatic temperature compensation.
+ * You can send commands in the serial monitor to execute the calibration.
+ * Serial Commands:
+ *   enterph -> enter the calibration mode
+ *   calph   -> calibrate with the standard buffer solution, two buffer solutions(4.0 and 7.0) will be automaticlly recognized
+ *   exitph  -> save the calibrated parameters and exit from calibration mode
+ *
+ * Copyright   [DFRobot](http://www.dfrobot.com), 2018
+ * Copyright   GNU Lesser General Public License
+ *
+ * version  V1.0
+ * date  2018-04
+ */
+ /*********
+  Rui Santos
+  Complete project details at https://randomnerdtutorials.com  
+  Based on the Dallas Temperature Library example
+*********/
+
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include "DFRobot_PH.h"
+#include <EEPROM.h>
+
+// Data wire is conntec to the Arduino digital pin 4
+#define ONE_WIRE_BUS 4
+
+#define PH_PIN A1
+
+// Setup a oneWire instance to communicate with any OneWire devices
+OneWire oneWire(ONE_WIRE_BUS);
+
+// Pass our oneWire reference to Dallas Temperature sensor 
+DallasTemperature sensors(&oneWire);
+
+float voltage,phValue,temperature = 25;
+DFRobot_PH ph;
+
+void setup()
+{
+    Serial.begin(115200);  
+    ph.begin();
+    sensors.begin();
+}
+
+void loop()
+{
+    static unsigned long timepoint = millis();
+    if(millis()-timepoint>1000U){                  //time interval: 1s
+        timepoint = millis();
+        temperature = sensors.getTempCByIndex(0);         // read your temperature sensor to execute temperature compensation
+        voltage = analogRead(PH_PIN)/1024.0*5000;  // read the voltage
+        phValue = ph.readPH(voltage,temperature);  // convert voltage to pH with temperature compensation
+        Serial.print("temperature:");
+        Serial.print(temperature,1);
+        Serial.print("^C  pH:");
+        Serial.println(phValue,2);
+    }
+    ph.calibration(voltage,temperature);           // calibration process by Serail CMD
+}
